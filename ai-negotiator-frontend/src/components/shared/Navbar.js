6bd8c29b6a80d07001c85d1f9fd5765e87641f6e
Ndyabagye Henry <ndyabagyehenrytusi@gmail.com>
2022-06-20 21:25:18 +0300

    1. Started on the navbar

---
+++ ai-negotiator-frontend/src/components/shared/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className='grid grid-cols-5 gap-2 bg-gray-300 h-10 p-3'>
+        <div id="logo" className='grid-cols-1'>BSSE22-9</div>
+        <div id="logo" className='grid-cols-3'>Nav Links</div>
+        <div id="logo" className='grid-cols-1'>Menu Stuff</div>
+    </nav>
+  )
+}
