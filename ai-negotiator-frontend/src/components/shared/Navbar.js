c5fb49e37f3dcd430caf950ff752a65f62361f07
Ndyabagye Henry <ndyabagyehenrytusi@gmail.com>
2022-06-20 23:32:27 +0300

    Product Cards
    1. Added product cards
    2. Included button component
    3. Created navbar

--- ai-negotiator-frontend/src/components/shared/Navbar.js
+++ ai-negotiator-frontend/src/components/shared/Navbar.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav className='grid grid-cols-5 gap-2 bg-gray-300 h-10 p-3'>
-        <div id="logo" className='grid-cols-1'>BSSE22-9</div>
-        <div id="logo" className='grid-cols-3'>Nav Links</div>
-        <div id="logo" className='grid-cols-1'>Menu Stuff</div>
+    <nav className='fixed w-full grid grid-cols-5 gap-2 bg-gray-100  shadow-md h-20 p-3 items-center'>
+        <div id="logo" className='grid-cols-1 font-medium text-green-500'>BSSE22-9</div>
+        <div id="logo" className='grid-cols-3 font-medium'>Nav Links</div>
+        <div id="logo" className='grid-cols-1 font-medium'>Menu Stuff</div>
     </nav>
   )
 }
