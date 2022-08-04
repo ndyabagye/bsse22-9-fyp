8f77905f1ead87895f083ab5347ca434f70844c5
KigulaJesse <jkigula@icloud.com>
2022-08-04 12:25:21 +0300

    one requirements file

--- negotiation/main.py
+++ negotiation/main.py
@@ -8,6 +8,9 @@ import numpy as np
 
 import nltk
 from nltk.stem import WordNetLemmatizer
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 
 from tensorflow.keras.models import load_model
 
