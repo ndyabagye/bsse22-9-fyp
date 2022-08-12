62df1005bceeff84ea6a6c51ed82fbbfd44aa1bd
KigulaJesse <jkigula@icloud.com>
2022-08-12 10:34:51 +0300

    first urls

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -1,14 +1,24 @@
 from . import views
 from django.urls import path
 
-
-
 app_name = 'product'
 
-
 urlpatterns = [
-    path('search', views.search, name="search"),
-    path('<slug:category_slug>/<slug:product_slug>/', views.product, name="product"),
-    path('<slug:category_slug>/', views.category, name="category"),
+
+
+
+    #---GET LIST OF PRODUCTS URLS
+    path('all_products', views.all_products, name="all_products"),
+    path('brand/<int:brand_id>', views.brand_products, name="brand_products"),
+    path('vendor/<int:vendor_id>', views.vendor_products, name="vendor_products"),
+    path('category/<int:category_id>', views.category_products, name="category_products"),
+    
+    #--GET A SINGULAR PRODUCT
+    path('<int:product_id>', views.product, name="product"),
+    
+    
+    #path('search', views.search, name="search"),
+    #path('<slug:category_slug>/<slug:product_slug>/', views.product, name="product"),
+    #path('<slug:category_slug>/', views.category, name="category")
     
 ]
--- backend/product/views.py
+++ backend/product/views.py
@@ -1,20 +1,60 @@
 import random # To get random products from the database
-from django.contrib import messages
 from django.shortcuts import redirect, render, get_object_or_404
-
-from .models import Category, Product
-
+from .models import Category, Product,Brand
 from django.db.models import Q
-
-from .forms import AddToCartForm
 from cart.cart import Cart
+from django.http import JsonResponse
 
 
-# Create your views here.
+#--- USED TO GET BRANDS---#
+def all_brands(request):
+    brands = list(Product.objects.values())
+    return JsonResponse(brands,safe=False)
+
+
+#--- USED TO GET CATEGORIES---#
+def all_categories(request):
+    categories = list(Category.objects.values())
+    return JsonResponse(categories,safe=False)
+
+def get_child_categories(request):
+    child_categories = list(Category.objects.values())
+    return JsonResponse(child_categories,safe=False)
+
+#--- USED TO GET PRODUCTS---#
+def all_products(request):
+    products = list(Product.objects.values())
+    return JsonResponse(products,safe=False)
+
+def brand_products(request,brand_id):
+    obj = Product.objects.filter(brand_id=brand_id)
+    products = list(obj.values())
+    return JsonResponse(products,safe=False)
+
+def vendor_products(request,vendor_id):
+    obj = Product.objects.filter(vendor_id = vendor_id)
+    products = list(obj.values())
+    return JsonResponse(products,safe=False)
+
+def category_products(request,category_id):
+    obj = Product.objects.filter(category_id=category_id)
+    products = list(obj.values())
+    return JsonResponse(products,safe=False)
+
+def product(request, product_id):
+    obj = Product.objects.filter(pk=product_id)
+    product = list(obj.values())
+    return JsonResponse(product,safe=False)
+
+
+
+
+
+"""# Create your views here.
 def product(request, category_slug, product_slug):
     # Create instance of Cart class
     cart = Cart(request)
-
+    
     product = get_object_or_404(Product, category__slug=category_slug, slug=product_slug)
 
     # Check whether the AddToCart button is clicked or not
@@ -51,9 +91,11 @@ def category(request, category_slug):
     category = get_object_or_404(Category, slug=category_slug)
     return render(request,'product/category.html', {'category': category})
 
-
 def search(request):
+    print("")
+    print("hehr")
+    print("")
     query = request.GET.get('query', '') # second is default parameter which is empty
     products = Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
 
-    return render(request, 'product/search.html', {'products':products, 'query': query})
+    #return render(request, 'product/search.html', {'products':products, 'query': query})"""
