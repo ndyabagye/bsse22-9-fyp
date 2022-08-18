167bd850c58f380397e2ba10ce3b717204d1ec4b
ndyabagye <ndyabagyehenrytusi@gmail.com>
2022-08-18 18:47:45 +0300

    users table

--- frontend/src/AdminBackend/pages/Users.js
+++ frontend/src/AdminBackend/pages/Users.js
@@ -1,78 +1,142 @@
-import React from "react";
-import { Table } from "flowbite-react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import BackendLayout from "../components/BackendLayout";
+import Loader from "../../Shared/Loader";
 
 export default function Users() {
-//   const [isMenu, setIsMenu] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [usersPerPage] = useState(10);
 
-//   const showMenu = () => {
-//     setIsMenu(!isMenu);
-//   };
+  useEffect(() => {
+    const fetchUsers = async() =>{
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/users/');
+      setUsers(res.data);
+      setLoading(false);
+    };
+    fetchUsers();
+  },[]);
+  //   const [isMenu, setIsMenu] = useState(false);
+
+  //   const showMenu = () => {
+  //     setIsMenu(!isMenu);
+  //   };
   return (
     <BackendLayout title={"Users"}>
-      <div className="flex flex-col">
-        {/* products table */}
-        <Table hoverable={true} className="mt-4">
-          <Table.Head>
-            <Table.HeadCell>User name</Table.HeadCell>
-            <Table.HeadCell>Email</Table.HeadCell>
-            <Table.HeadCell>Phone Number</Table.HeadCell>
-            <Table.HeadCell>Address</Table.HeadCell>
-            <Table.HeadCell>
-              <span>Actions</span>
-            </Table.HeadCell>
-          </Table.Head>
-          <Table.Body className="divide-y">
-            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                Apple MacBook Pro 17"
-              </Table.Cell>
-              <Table.Cell>Sliver</Table.Cell>
-              <Table.Cell>Laptop</Table.Cell>
-              <Table.Cell>$2999</Table.Cell>
-              <Table.Cell>
+      <div className="overflow-x-auto relative shadow-md sm:rounded-lg p-2">
+        <div className="flex justify-between items-center px-3">
+        <label for="table-search" className="sr-only">
+  Search
+</label>
+<div className="relative">
+  <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+    <svg
+      className="w-5 h-5 text-gray-500 dark:text-gray-400"
+      aria-hidden="true"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+        clip-rule="evenodd"
+      ></path>
+    </svg>
+  </div>
+  <input
+    type="text"
+    id="table-search-users"
+    className="block p-2 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    placeholder="Search for users"
+  />
+</div>
+        </div>
+        {/* table */}
+        {loading && <Loader/>}
+        {!loading && <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="p-4">
+                <div className="flex items-center">
+                  <input
+                    id="checkbox-all-search"
+                    type="checkbox"
+                    className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                  <label for="checkbox-all-search" className="sr-only">
+                    checkbox
+                  </label>
+                </div>
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Name
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Phone
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Company
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {users?.map((user) => (
+              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key="user?.id">
+              <td className="p-4 w-4">
+                <div className="flex items-center">
+                  <input
+                    id="checkbox-table-search-1"
+                    type="checkbox"
+                    className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                  <label for="checkbox-table-search-1" className="sr-only">
+                    checkbox
+                  </label>
+                </div>
+              </td>
+              <th
+                scope="row"
+                className="flex items-center py-4 px-6 text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                <img
+                  className="w-10 h-10 rounded-full"
+                  src='https://via.placeholder.com/150'
+                  alt=""
+                />
+                <div className="pl-3">
+                  <div className="text-base font-semibold">{user?.name}</div>
+                  <div className="font-normal text-gray-500">
+                    {user?.email}
+                  </div>
+                </div>
+              </th>
+              <td className="py-4 px-6">{user?.phone}</td>
+              <td className="py-4 px-6">
+                <div className="flex items-center">
+                  {user?.company?.name}
+                </div>
+              </td>
+              <td className="py-4 px-6">
                 <a
-                  href="/tables"
-                  className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                  href="#"
+                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
                 >
-                  Edit
+                  Edit user
                 </a>
-              </Table.Cell>
-            </Table.Row>
-            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                Microsoft Surface Pro
-              </Table.Cell>
-              <Table.Cell>White</Table.Cell>
-              <Table.Cell>Laptop PC</Table.Cell>
-              <Table.Cell>$1999</Table.Cell>
-              <Table.Cell>
-                <a
-                  href="/tables"
-                  className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  Edit
-                </a>
-              </Table.Cell>
-            </Table.Row>
-            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                Magic Mouse 2
-              </Table.Cell>
-              <Table.Cell>Black</Table.Cell>
-              <Table.Cell>Accessories</Table.Cell>
-              <Table.Cell>$99</Table.Cell>
-              <Table.Cell>
-                <a
-                  href="/tables"
-                  className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  Edit
-                </a>
-              </Table.Cell>
-            </Table.Row>
-          </Table.Body>
-        </Table>
+              </td>
+            </tr>
+            ))};
+
+
+          </tbody>
+        </table>}
       </div>
     </BackendLayout>
   );
