d9aefa31b489b4f952909048c6eb0d9bc91f1300
ssembatyaisaac <ssembatyaisaac1999@gmail.com>
2022-08-23 17:58:30 +0300

    Redirected localhost:8000 to react homepage

--- backend/simple_multivendor_site/urls.py
+++ backend/simple_multivendor_site/urls.py
@@ -6,12 +6,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('admin/', admin.site.urls),
     path('vendor/', include('vendor.urls')),
     path('product/', include('product.urls')),
     path('cart/', include('cart.urls')),
     path('order/', include('order.urls')),
     path('location/', include('location.urls')),
-    path('api/v1/users/', include('users.urls')),
+    path('users/', include('users.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
