fa497e33fba807ce17eb3554f5116b9222ce8d6b
KigulaJesse <jkigula@icloud.com>
2022-08-31 16:54:45 +0300

    many_urls

--- backend/product/forms.py
+++ backend/product/forms.py
@@ -7,8 +7,45 @@ class AddToCartForm(forms.Form):
     quantity = forms.IntegerField()
 
 class CategoryForm(forms.Form):
-    title = forms.CharField(label='title', max_length=100)
+    name = forms.CharField(label='name', max_length=100)
     slug = forms.CharField(label='slug', max_length=100)
     description = forms.CharField(label='description', max_length=100)
-    featured = forms.BooleanField(required=False,initial=False)
-    menu = forms.BooleanField(required=False,initial=False)
+    status = forms.BooleanField(required=False, initial=False)
+
+class BrandForm(forms.Form):
+    name = forms.CharField(label='name', max_length=100)
+    slug = forms.CharField(label='slug', max_length=100)
+    description = forms.CharField(label='description', max_length=100)
+    status = forms.BooleanField(required=False, initial=False)
+
+"""class ProductForm(forms.Form):
+    name = forms.CharField(label='name', max_length=100)
+    slug = forms.CharField(label='slug', max_length=100)
+    description = forms.CharField(label='description', max_length=100)
+    status = forms.BooleanField(required=False, initial=False)
+    
+    category_id = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    vendor_id = models.ForeignKey(Vendor, related_name="products", on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    added_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    image2 = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    image3 = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    status = models.BooleanField(default=False)
+    
+    TRANSMISSION_CHOICES = [
+        ('automatic', 'Automatic'),
+        ('manual', 'Manual'),
+    ]
+    selling_price = models.IntegerField(default=0,blank=True, null=True)
+    base_price = models.IntegerField(default=0,decimal_places=2, blank=True, null=True)
+    profit = models.IntegerField(default=0,decimal_places=2, blank=True, null=True)
+    brand_id = models.ForeignKey(Brand, related_name='products', blank=True, null=True, on_delete=models.SET_NULL)
+    model = models.CharField(max_length=50)
+    transmission = models.CharField(
+        max_length='50',
+        choices=TRANSMISSION_CHOICES,
+        default="automatic"
+    )
+    year = models.IntegerField(default=0)
+    mileage = models.IntegerField(default=0)"""
--- backend/product/urls.py
+++ backend/product/urls.py
@@ -4,19 +4,32 @@ from django.urls import path
 app_name = 'product'
 
 urlpatterns = [
-
-
-
-    #---GET LIST OF PRODUCTS URLS
-    path('all_products', views.all_products, name="all_products"),
-    path('all_categories', views.all_categories, name="all_categories"),
     
-    #--GET A SINGULAR OBJECT
+    #---BRAND URLS --- all urls that return brand objects
+    path('all_brands', views.all_brands, name="All Brands"),
+    path('create_brand', views.create_brand, name="Create Brand"),
+    path('brand/<int:brand_id>', views.get_brand, name="Get Brand"),
+    path('update_brand/<int:brand_id>', views.update_brand, name="Update Brand"),
+    path('archive_brand/<int:brand_id>', views.archive_brand, name="Archive Brand"),
     
-    path('brand/<int:brand_id>', views.brand_products, name="brand_products"),
-    path('vendor/<int:vendor_id>', views.vendor_products, name="vendor_products"),
-    path('category/<int:category_id>', views.category_products, name="category_products"),
-    path('<int:product_id>', views.single_product, name="product"),
+    
+    #---CATEGORY URLS -- all urls that return category objects
+    path('all_categories', views.all_categories, name="All Categories"),
+    path('create_category', views.create_category, name="Create Category"),
+    path('category/<int:category_id>', views.get_category, name="Get Category"),
+    path('update_category/<int:category_id>', views.update_category, name="Update Category"),
+    path('archive_category/<int:category_id>', views.archive_category, name="Archive Category"),
+
+
+    #---PRODUCTS URLS --- all urls that return product objects
+    path('all', views.all_products, name="all_products"),
+    path('create', views.create_product, name="Create Product"),
+    path('<int:product_id>', views.get_product, name="Get Product"),
+    path('update/<int:product_id>', views.update_product, name="Update Product"),
+    path('archive/<int:product_id>', views.archive_product, name="Archive Product"),
+    path('brand_products/<int:brand_id>', views.brand_products, name="Brand Products"),
+    path('vendor_products/<int:vendor_id>', views.vendor_products, name="Vendor Products"),
+    path('category_products/<int:category_id>', views.category_products, name="Category Products"),
     
     #--PREDICT A  PRICE--#
     path('predictPrice', views.predict_price, name="predict_price"),
@@ -24,10 +37,10 @@ urlpatterns = [
     #---SEND TO CHAT---#
     path('sendChat', views.send_chat, name="send_chat"),
 
-    #---POST LIST OF PRODUCTS---#
-    path('storeCategory', views.store_category, name="store_category"),
-    path('storeBrand', views.store_brand, name="send_chat"),
 
+
+
+    #---OLD URLS---#
     path('search', views.search, name="search"),
     path('<slug:category_slug>/<slug:product_slug>/', views.product, name="product"),
     path('<slug:category_slug>/', views.category, name="category")
